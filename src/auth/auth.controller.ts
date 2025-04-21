1d38a410d78cad6409c403563259623d86f10fa1
Eugene Posokhin <evgenii.posokhin@gmail.com>
2025-04-22 00:40:53 +0600

    feat: add dto validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,20 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @Post('login')
   login(@Body() loginUserDto: LoginUserDto) {
-    // return 'login'``
     return this.authService.login(loginUserDto);
   }
-  
+
+  @UsePipes(new ValidationPipe())
   @Post('registration')
   registration(@Body() createUserDto: CreateUserDto) {
-    // return 'registration' 
     return this.authService.registration(createUserDto);
   }
 
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsString, Length } from "class-validator"
+
 export class CreateUserDto {
+    @IsString()
+    @Length(4, 16)
     readonly login: string
+    @IsEmail()
     readonly email: string
+    @IsString()
+    @Length(4, 32)
     readonly password: string
 }
